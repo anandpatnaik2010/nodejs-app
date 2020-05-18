9f118938ceb7564717e948936af6790e1040f1fa
anandpatnaik <anandpatnaik2010@gmail.com>
2020-05-18 16:34:08 +0530

    updated terraform file

--- terraform/ec2-machine.tf
+++ terraform/ec2-machine.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkins" {
   instance_type               = "${var.instance_type}"
   subnet_id                   = "${var.subnet_id["public-0"]}"
   vpc_security_group_ids      = ["${aws_security_group.jenkins.id}"]
-
+  key_name                    = "anand-test"
 }
 
 resource "aws_security_group" "jenkins" {
